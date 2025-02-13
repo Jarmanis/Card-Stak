abc91bfb2a8a09fad99a486167af828abae9298b
SwankyJ <7084290-SwankyJ@users.noreply.replit.com>
2025-02-13 04:29:47 +0000

    User checkpoint: Implement user authentication using React-oidc-context and AWS Cognito.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import './App.css'
+import { useAuth } from "react-oidc-context";
 
-export default function App() {
+function App() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "5gh0dubj45gj1oo07ptsiu05ks";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+  );
 }
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const cognitoAuthConfig = {
+	authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nuni86pdc",
+	client_id: "5gh0dubj45gj1oo07ptsiu05ks",
+	redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+	response_type: "code",
+	scope: "phone openid email",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
 	<React.StrictMode>
-		<App />
+		<AuthProvider {...cognitoAuthConfig}>
+			<App />
+		</AuthProvider>
 	</React.StrictMode>
-)
+);
