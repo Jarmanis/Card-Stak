c274b4170c29fce1475c78d20d91f757eb0de046
SwankyJ <7084290-SwankyJ@users.noreply.replit.com>
2025-02-13 04:44:47 +0000

    User checkpoint: Update authentication and implement basic transaction submission form.  Adds user authentication using React-oidc-context and a form for submitting transaction data.

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -6,9 +7,9 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
 	authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nuni86pdc",
 	client_id: "5gh0dubj45gj1oo07ptsiu05ks",
-	redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+	redirect_uri: "https://77348dac-e223-436e-a868-fb97b4acfb58-00-2swqdgk0l80qr.riker.replit.dev/",
 	response_type: "code",
-	scope: "phone openid email",
+	scope: "email openid phone",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
