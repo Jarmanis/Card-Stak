da76cef1c2d35c59ebee262800038b22322df760
Jay <49914802+Jarmanis@users.noreply.github.com>
2025-02-13 04:36:39 +0000

    Restored to '569a44d5e1fc681e409ee739037fcb5441324207'
    Replit-Restored-To:569a44d5e1fc681e409ee739037fcb5441324207

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,11 @@ import App from "./App";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-	authority: import.meta.env.VITE_COGNITO_AUTHORITY || "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nuni86pdc",
-	client_id: import.meta.env.VITE_COGNITO_CLIENT_ID || "5gh0dubj45gj1oo07ptsiu05ks",
-	redirect_uri: `${window.location.origin}/callback`,
+	authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nuni86pdc",
+	client_id: "5gh0dubj45gj1oo07ptsiu05ks",
+	redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
 	response_type: "code",
-	scope: "openid email profile",
-	loadUserInfo: true
+	scope: "phone openid email",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
