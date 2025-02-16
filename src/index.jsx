645b688f3c1d71e0dd4764c31ce1598ba80be1be
SwankyJ <7084290-SwankyJ@users.noreply.replit.com>
2025-02-16 05:16:40 +0000

    User checkpoint: Fix: Correct redirect URI for user authentication to resolve login error.

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
 	authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nuni86pdc",
 	client_id: "5gh0dubj45gj1oo07ptsiu05ks",
-	redirect_uri: "https://77348dac-e223-436e-a868-fb97b4acfb58-00-2swqdgk0l80qr.riker.replit.dev/",
+	redirect_uri: "https://card-stak.replit.app/",
 	response_type: "code",
 	scope: "email openid phone",
 };
