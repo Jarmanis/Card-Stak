fbf1648c891890cc050cf6012f12592734f4946d
Jarmani Soriano <jarmani@Jarmanis-MacBook-Pro.local>
2025-06-26 01:55:03 -0400

    Add deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['77348dac-e223-436e-a868-fb97b4acfb58-00-2swqdgk0l80qr.riker.replit.dev']
-  }
+  
 })
