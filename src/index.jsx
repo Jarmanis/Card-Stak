27a59cb12a33bc6d9bd60344eca43a810e26cc83
Jarmani Soriano <jarmani@Jarmanis-MacBook-Pro.local>
2025-06-28 01:13:59 -0400

    update env variables

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-	authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_nuni86pdc",
+	authority: "https://us-east-1nuni86pdc.auth.us-east-1.amazoncognito.com",
 	client_id: "5gh0dubj45gj1oo07ptsiu05ks",
 	redirect_uri: "https://card-stak.com",
 	response_type: "code",
